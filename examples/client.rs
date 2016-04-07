fcc0282edcb1007f3564b1f1ee4f01946bb1c858
Joe Wilm <joe@jwilm.com>
2016-04-07 10:26:18 -0700

    Buffering handler in client

--- examples/client.rs
+++ examples/client.rs
@@ -12,16 +12,28 @@ use hyper::header::Connection;
 use hyper::{Decoder, Encoder, Next};
 use hyper::net::DefaultTransport as HttpStream;
 
+enum Message {
+    Response(Response, Vec<u8> /* body */),
+    Error(String),
+}
 
-struct Dump(mpsc::Sender<()>);
+struct BufferingHandler {
+    tx: mpsc::Sender<Message>,
+    buf: Vec<u8>,
+    response: Option<Response>,
+}
 
-impl Drop for Dump {
-    fn drop(&mut self) {
-        let _ = self.0.send(());
+impl BufferingHandler {
+    pub fn new(tx: mpsc::Sender<Message>) -> Self {
+        BufferAndSend {
+            tx: tx,
+            buf: Vec::new(),
+            response: None,
+        }
     }
 }
 
-impl hyper::client::Handler<HttpStream> for Dump {
+impl hyper::client::Handler<HttpStream> for BufferingHandler {
     fn on_request(&mut self, req: &mut Request) -> Next {
         req.headers_mut().set(Connection::close());
         Next::read()
@@ -31,18 +43,25 @@ impl hyper::client::Handler<HttpStream> for Dump {
         Next::read()
     }
 
-    fn on_response(&mut self, _res: Response) -> Next {
+    fn on_response(&mut self, res: Response) -> Next {
+        self.response = Some(res);
         Next::read()
     }
 
     fn on_response_readable(&mut self, decoder: &mut Decoder<HttpStream>) -> Next {
-        match io::copy(decoder, &mut io::stdout()) {
-            Ok(0) => Next::end(),
+        match io::copy(decoder, &mut self.buf) {
+            Ok(0) => {
+                let mut buf = Vec::new();
+                ::std::mem::swap(&mut self.buf, &mut buf);
+                let resp = self.response.take().expect("Response received and not consumed");
+                self.tx.send(Message::Response(resp, buf)).ok();
+                Next::end()
+            },
             Ok(_) => Next::read(),
             Err(e) => match e.kind() {
                 io::ErrorKind::WouldBlock => Next::read(),
                 _ => {
-                    println!("ERROR: {}", e);
+                    self.tx.send(Message::Error(format!("ERROR: {}", e))).ok();
                     Next::end()
                 }
             }
@@ -68,10 +87,10 @@ fn main() {
 
     let (tx, rx) = mpsc::channel();
     let client = Client::new().expect("Failed to create a Client");
-    let to_send = 2000;
+    let to_send = 200;
 
     for _ in 0..to_send {
-        client.request(url.parse().unwrap(), Dump(tx.clone())).unwrap();
+        client.request(url.parse().unwrap(), BufferingHandler::new(tx.clone())).unwrap();
     }
 
     let sent = to_send;
@@ -79,7 +98,14 @@ fn main() {
     let mut i = 0;
     let mut received = 0;
     loop {
-        if let Ok(_) = rx.try_recv() {
+        if let Ok(msg) = rx.try_recv() {
+            match msg {
+                Message::Error(err) => println!("{}", err),
+                Message::Response(_resp, body) => {
+                    let mut cursor = io::Cursor::new(body);
+                    io::copy(&mut cursor, &mut io::stdout()).ok();
+                }
+            }
             received += 1;
             i = 0;
             if received == sent {
